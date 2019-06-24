282c32887e07a9e752367b7d8899e9e5829326d9
Nicholas Currault <nicholas.curr+dev@gmail.com>
2019-06-24 00:03:46 -0700

    re-add feedback, error handlers

--- main.py
+++ main.py
@@ -27,6 +27,36 @@ def get_static_handler(command):
         ( lambda bot, update : \
         bot.send_message(chat_id=update.message.chat.id, text=response) ) )
 
+# Credit: https://github.com/CaKEandLies/Telegram_Cthulhu/blob/master/cthulhu_game_bot.py#L63
+def feedback_handler(bot, update, args=None):
+    """
+    Store feedback from users in a text file.
+    """
+    if args and len(args) > 0:
+        feedback = open("ignore/feedback.txt", "a")
+        feedback.write("\n")
+        feedback.write(update.message.from_user.first_name)
+        feedback.write("\n")
+        # Records User ID so that if feature is implemented, can message them
+        # about it.
+        feedback.write(str(update.message.from_user.id))
+        feedback.write("\n")
+        feedback.write(" ".join(args))
+        feedback.write("\n")
+        feedback.close()
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Thanks for the feedback!")
+    else:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Format: /feedback [feedback]")
+
+def handle_error(bot, update, error):
+    try:
+        raise error
+    except TelegramError:
+        logging.getLogger(__name__).warning('TelegramError! %s caused by this update: %s', error, update)
+
+
 active_polls = {}
 
 class InvalidInput(Exception):
