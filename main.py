a18ce6b7463052bf87d46b7acfce8d2adbb5086c
Nicholas Currault <nicholas.curr+dev@gmail.com>
2019-06-26 00:50:47 -0700

    simple poll viewer, more command handlers

--- main.py
+++ main.py
@@ -59,19 +59,26 @@ def handle_error(bot, update, error):
     except TelegramError:
         logging.getLogger(__name__).warning('TelegramError! %s caused by this update:\n%s', error, update)
 
-
-active_polls = {}
-
 class InvalidInput(Exception):
     pass
 
 class Poll:
-    def __init__(self, question, first_option):
+    def __init__(self, question, options, live_results):
         self.question = question
-        self.options = [option]
+        self.live_results = live_results
+        self.options = options
+        self.votes = {}
 
-    def add_vote(self, user, first_choices):
-        self.votes[user] = Vote(first_choices)
+    def __str__(self):
+        poll_type = "live ranked-pairs poll" if self.live_results else "ranked-pairs poll with results at end"
+        option_lines = '\n'.join( map(lambda o: f"▫️ {o}\n", self.options) )
+        return f"<b>{self.question}</b>\n" + \
+            f"<i>{poll_type}</i>\n\n" + option_lines
+        # TODO checked boxes next to winner(s)
+
+    def add_vote(self, user):
+        if user not in self.votes:
+            self.votes[user] = Vote(user, self)
         return self.votes[user]
 
     def call_election(self):
@@ -84,12 +91,13 @@ class Poll:
 class Vote:
     def __init__(self, user, poll):
         self.poll = poll
-        self.option_rankings = [0] * len(poll.options)
+        self.n_options = len(poll.options)
+        self.option_rankings = [0] * self.n_options
         self.current_rank = 1
         self.finalized = False
 
     def inc_current_rank(self):
-        self.current_rank = (self.current_rank + 1) % len(poll.options)
+        self.current_rank = (self.current_rank + 1) % self.n_options
 
     def toggle_ranking(self, option):
         if option < 0 or option >= n_options:
@@ -117,26 +125,88 @@ class CreationStatus(Enum):
     WRITING_OPTIONS = 4
 
 
+active_polls = {}
+
 def new_poll_handler(bot, update, user_data):
     if update.message.chat.type == "private":
+        user_data["create_status"] = CreationStatus.CHOOSING_RESULT_TYPE
+        keyboard_options = telegram.ReplyKeyboardMarkup(keyboard=[["Live Results", "When Closed"]], resize_keyboard=True)
+
         bot.send_message(chat_id=update.message.chat.id,
             text="Let's make a ranked-pairs poll! Send /cancel at any time to stop.")
         bot.send_message(chat_id=update.message.chat.id,
-            text="Would you like the poll results to appear live or only when it is closed?")
-        # TODO custom keyboard replies: "Live Results" or "When Closed"
-
-        chat_data["create_status"] = CreationStatus.CHOOSING_RESULT_TYPE
+            text="Would you like the poll results to appear live or only when it is closed?",
+            reply_markup=keyboard_options)
     else:
         update.message.reply_markdown(text=f"Don't spam this chat, [slide into my DMs](t.me/{USERNAME}) to start a poll.")
 
 def poll_done_handler(bot, update, user_data):
-    pass # TODO
+    status = user_data.get("create_status")
+    if status == CreationStatus.WRITING_OPTIONS:
+        if len(user_data["pending_options"]) >= 2:
+            poll = Poll(user_data["pending_question"],
+                user_data["pending_options"], user_data["pending_results_live"])
+            poll_id = uuid.uuid4()
+            global active_polls
+            active_polls[poll_id] = poll
+
+            bot.send_message(chat_id=update.message.chat.id,
+                text=f"Successfully created poll!")
+
+            bot.send_message(chat_id=update.message.chat.id,
+                text=str(poll), parse_mode=telegram.ParseMode.HTML)
+
+            # TODO inline interface to show a summary of options, share, and close
+
+            user_data["create_status"] = CreationStatus.WAITING # now waiting for another poll
+
+            return
+        else:
+            reason = "write at least two options"
+    elif status == CreationStatus.CHOOSING_RESULT_TYPE:
+        reason = "chose a result type"
+    elif status == CreationStatus.WRITING_QUESTION:
+        reason = "write a question"
+    else:
+        reason = "start a poll with /start"
+
+    bot.send_message(chat_id=update.message.chat.id,
+        text=f"Cannot create poll. Please {reason}!")
 
 def cancel_handler(bot, update, user_data):
-    pass # TODO
+    if user_data.get("create_status") != CreationStatus.WAITING:
+        user_data["create_status"] = CreationStatus.WAITING
+
+        bot.send_message(chat_id=update.message.chat.id,
+            text="Cancelled! /start to try again",
+            reply_markup=telegram.ReplyKeyboardRemove())
 
 def message_handler(bot, update, user_data):
-    pass # TODO
+    if "create_status" in user_data:
+        if user_data["create_status"] == CreationStatus.CHOOSING_RESULT_TYPE:
+            if update.message.text in ("Live Results", "When Closed"):
+                user_data["pending_results_live"] = update.message.text == "Live Results"
+                user_data["create_status"] = CreationStatus.WRITING_QUESTION
+
+                bot.send_message(chat_id=update.message.chat.id,
+                    text="Great! Now send the question for your poll",
+                    reply_markup=telegram.ReplyKeyboardRemove())
+            else:
+                update.message.reply_text("Please reply using the buttons")
+                return
+            # reply_markup=telegram.ReplyKeyboardRemove()
+        elif user_data["create_status"] == CreationStatus.WRITING_QUESTION:
+            user_data["create_status"] = CreationStatus.WRITING_OPTIONS
+            user_data["pending_question"] = update.message.text
+            user_data["pending_options"] = []
+            bot.send_message(chat_id=update.message.chat.id,
+                text="Great! Now send the first option")
+        elif user_data["create_status"] == CreationStatus.WRITING_OPTIONS:
+            user_data["pending_options"].append(update.message.text)
+            msg = "Great! Now send another option"
+            if len(user_data["pending_options"]) >= 2:
+                msg = msg + " (or /done if there are no more options)"
+            bot.send_message(chat_id=update.message.chat.id, text=msg)
 
 # TODO more handlers for button presses
 
@@ -149,7 +219,7 @@ if __name__ == "__main__":
     dispatcher.add_handler(CommandHandler('feedback', feedback_handler, pass_args=True))
 
     dispatcher.add_handler(CommandHandler('start', new_poll_handler, pass_user_data=True))
-    dispatcher.add_handler(CommandHandler('done', new_poll_handler, pass_user_data=True))
+    dispatcher.add_handler(CommandHandler('done', poll_done_handler, pass_user_data=True))
     dispatcher.add_handler(CommandHandler('cancel', cancel_handler, pass_user_data=True))
 
     dispatcher.add_handler(MessageHandler(Filters.text, message_handler, pass_user_data=True))
