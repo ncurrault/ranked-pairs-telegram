86cd620ff575c5c4d08d97824c5c2ad47685c640
Nicholas Currault <nicholas.curr+dev@gmail.com>
2019-07-17 00:31:17 -0700

    begin re-implementing ranked pairs

---
+++ ranked_pairs.py
@@ -0,0 +1,7 @@
+def get_winners(ballots):
+    return {0} # TODO implement
+
+# TODO basically rank the set of candidates
+def get_ranked_partitions(ballots):
+    return [ {b} for b in range(len(ballots[0])) ]
+    # TODO implement
